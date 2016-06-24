cc94f0dd3cbc6973e744a41309ad6707291cf13f
Adam Nawrocki <adam.nawroc@gmail.com>
2016-06-24 09:35:32 -0400

    adjust naming

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(function() {
     $("nav ul").toggle()
   })
 
-  $(".before-after").hover(function() {
+  $(".broadway-before-after").hover(function() {
     $(this).attr("src", "images/broadway.png")
   }, function() {
      $(this).attr("src", "images/before.png")
