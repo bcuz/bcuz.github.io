91cb9636de300a43073f0525a0196e18bd754570
Adam Nawrocki <adam.nawroc@gmail.com>
2016-08-30 13:38:01 -0400

    add missing semicols

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ $(function() {
 
   $("nav img").click(function(event) {
     event.stopPropagation();
-    $("nav ul").toggle()
-  })
+    $("nav ul").toggle();
+  });
 
   $(document).click( function(){
     var viewportWidth = $(window).width();
@@ -32,26 +32,26 @@ $(function() {
 });
 
   $(".broadway-before-after").hover(function() {
-    $(this).attr("src", "img/broadway.png")
+    $(this).attr("src", "img/broadway.png");
   }, function() {
-     $(this).attr("src", "img/before-broad.png")
-   })
+     $(this).attr("src", "img/before-broad.png");
+   });
 
 
   $(".bestbite-before").hover(function() {
-    $(this).attr("src", "img/bestbite.png")
+    $(this).attr("src", "img/bestbite.png");
   }, function() {
-     $(this).attr("src", "img/before-bestbite.png")
-   })
+     $(this).attr("src", "img/before-bestbite.png");
+   });
 
 
-  var no_opacity = ["img/bestbite.png", "img/before-bestbite.png", "img/before-broad.png", "img/broadway.png"]
+  var no_opacity = ["img/bestbite.png", "img/before-bestbite.png", "img/before-broad.png", "img/broadway.png"];
 
   $(".project img").hover(function() {
     if (no_opacity.indexOf($(this).attr("src")) === -1) {
-      $(this).toggleClass("opaque")
+      $(this).toggleClass("opaque");
     }
-  })
+  });
 
 
 });
