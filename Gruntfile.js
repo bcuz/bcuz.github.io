22eeaa5dca9ae97dda006cd0e1479fac0ed85a5e
Adam Nawrocki <adam.nawroc@gmail.com>
2016-09-12 14:41:09 -0400

    Revert "change raw img folde name"
    This reverts commit 4faacccbfc60c5ce0c072818a07eb9f88a1669b8.

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,7 @@ uglify: {
           // Set to true to enable the following options…
           expand: true,
           // cwd is 'current working directory'
-          cwd: 'raw-img',
+          cwd: 'img-raw',
           src: ['*.{png,jpg,svg}'],
           // Could also match cwd line above. i.e. project-directory/img/
           dest: 'img/',
