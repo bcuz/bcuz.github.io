56a9dd1924879e161f6cae058ba5fcbf01656258
Adam Nawrocki <adam.nawroc@gmail.com>
2016-10-05 07:19:31 -0400

    delete unnecessary code

--- js/app.js
+++ js/app.js
@@ -53,20 +53,6 @@ $(function() {
 
 });
 
-  // $(".broadway-before-after").hover(function() {
-  //   $(this).attr("src", "img/broadway.png");
-  // }, function() {
-  //    $(this).attr("src", "img/before-broad.png");
-  //  });
-
-
-  // $(".bestbite-before").hover(function() {
-  //   $(this).attr("src", "img/bestbite.png");
-  // }, function() {
-  //    $(this).attr("src", "img/before-bestbite.png");
-  //  });
-
-
   var no_opacity = [];
 
   $(".project img").hover(function() {
