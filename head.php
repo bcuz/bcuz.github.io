f616bbe50b769de12aee0488ad71e824a8287b6c
adam <adam.nawroc@gmail.com>
2016-11-17 08:54:09 -0500

    use include for head element

---
+++ head.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Adam Nawrocki - Front End Developer</title>
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
