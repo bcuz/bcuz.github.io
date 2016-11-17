743a7da8e05036ef0b587d4762091a6fef1d9798
adam <adam.nawroc@gmail.com>
2016-11-17 08:54:49 -0500

    head include port page

--- portfolio.php
+++ portfolio.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Adam Nawrocki - Front End Developer</title>
-    <link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+<?php include 'head.php' ?>
 
 <body>
     <nav>
