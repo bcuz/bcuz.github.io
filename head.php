739387fcb6c504c709aa51dc53a2dd59641e72da
adam <adam.nawroc@gmail.com>
2016-11-18 08:39:53 -0500

    playing with php var

--- head.php
+++ head.php
@@ -1,7 +1,8 @@
+<?php $title = "Adam Nawrocki - Front End Developer"; ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Adam Nawrocki - Front End Developer</title>
+    <title><?php echo $title ?></title>
     <link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
