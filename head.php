f28e267e7aa62ab757b054e4e2648931a65b15fb
adam <adam.nawroc@gmail.com>
2016-12-01 07:54:04 -0500

    changing title of port page

--- head.php
+++ head.php
@@ -1,4 +1,11 @@
-<?php $title = "Adam Nawrocki - Front End Developer"; ?>
+<?php
+
+$title = "Adam Nawrocki - Front End Developer";
+
+if (basename($_SERVER['PHP_SELF']) === "portfolio.php") {
+  $title = "Portfolio";
+}
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
